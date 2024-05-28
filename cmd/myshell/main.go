78f926e68db8d5d4e342c5163839f8f23524448c
Jye Harry <jyeharry21@hotmail.com>
2024-05-28 23:43:35 +1000

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,11 +4,24 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
+	"strings"
 )
+
+var Commands = []string{}
 
 func main() {
 	fmt.Fprint(os.Stdout, "$ ")
 
+	reader := bufio.NewReader(os.Stdin)
 	// Wait for user input
-	bufio.NewReader(os.Stdin).ReadString('\n')
+	command, err := reader.ReadString('\n')
+
+	if err != nil {
+		fmt.Println(err)	
+	} 
+
+	if !slices.Contains(Commands, command) {
+		fmt.Printf("%s: command not found\n", strings.Trim(command, "\n"))
+	}
 }
