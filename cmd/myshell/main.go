7658136d8e6a919590c0fbb4f9f6694428474a04
Jye Harry <jyeharry21@hotmail.com>
2024-05-29 00:53:47 +1000

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strconv"
 	"strings"
 )
@@ -11,7 +12,10 @@ import (
 const (
 	echo = "echo"
 	exit = "exit"
+	typeCmd = "type"
 )
+
+var builtins = []string{echo, exit, typeCmd}
 
 func main() {
 	reader := bufio.NewReader(os.Stdin)
@@ -37,6 +41,13 @@ func main() {
 				os.Exit(0)
 			}
 			os.Exit(code)
+		case typeCmd:
+			found := slices.Contains(builtins, args)
+			if found {
+				fmt.Printf("%s is a shell builtin\n", args)
+			} else {
+				fmt.Printf("%s not found\n", args)
+			}
 		default:
 			fmt.Printf("%s: command not found\n", strings.Trim(input, "\n"))
 		}
