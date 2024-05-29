e0f6df7d5ad564fbc044cba05f1ad03b2a864cf7
Jye Harry <jyeharry21@hotmail.com>
2024-05-29 12:45:35 +1000

    codecrafters submit [skip ci]

--- cmd/myshell/main.go
+++ cmd/myshell/main.go
@@ -4,14 +4,15 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"path/filepath"
 	"slices"
 	"strconv"
 	"strings"
 )
 
 const (
-	echo = "echo"
-	exit = "exit"
+	echo    = "echo"
+	exit    = "exit"
 	typeCmd = "type"
 )
 
@@ -29,27 +30,45 @@ func main() {
 			fmt.Println(err)
 		}
 
-		command, args, _ := strings.Cut(strings.Trim(input, "\n"), " ")
+		handleCommand(input)
+	}
+}
 
-		switch command {
-		case echo:
-			fmt.Println(args)		
-		case exit:
-			code, err := strconv.Atoi(args)
-			if err != nil {
-				fmt.Println("not a valid exit code")
-				os.Exit(0)
-			}
-			os.Exit(code)
-		case typeCmd:
-			found := slices.Contains(builtins, args)
-			if found {
-				fmt.Printf("%s is a shell builtin\n", args)
-			} else {
-				fmt.Printf("%s not found\n", args)
-			}
-		default:
-			fmt.Printf("%s: command not found\n", strings.Trim(input, "\n"))
+func handleCommand(input string) {
+	command, args, _ := strings.Cut(strings.Trim(input, "\n"), " ")
+	switch command {
+	case echo:
+		fmt.Println(args)
+	case exit:
+		code, err := strconv.Atoi(args)
+		if err != nil {
+			fmt.Println("not a valid exit code")
+			os.Exit(0)
+		}
+		os.Exit(code)
+	case typeCmd:
+		if slices.Contains(builtins, args) {
+			fmt.Printf("%s is a shell builtin\n", args)
+		} else if path, found := findExecutable(args); found {
+			fmt.Println(path)
+		} else {
+			fmt.Printf("%s not found\n", args)
+		}
+	default:
+		fmt.Printf("%s: command not found\n", strings.Trim(input, "\n"))
+	}
+}
+
+func findExecutable(cmd string) (string, bool) {
+	osPath := os.Getenv("PATH")
+	paths := strings.Split(osPath, ":")
+
+	for _, path := range paths {
+		fullpath := filepath.Join(path, cmd)
+		if _, err := os.Stat(fullpath); err == nil {
+			return fullpath, true
 		}
 	}
+
+	return "", false
 }
